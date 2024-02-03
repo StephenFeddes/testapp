009cf731173eeec110caa0073dc5f1f64d975a33
StephenFeddes <feddessteve@gmail.com>
2024-02-02 20:47:25 -0600

    Testing

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-// testing
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-app.get('/', (req, res) => res.send('Hello World!'));
+
+app.use((req, res, next) => {
+    console.log(`Incoming request to ${req.url}`);
+    next();
+});
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
